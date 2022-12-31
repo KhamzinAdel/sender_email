d9aa6905462b5679469d5f8788118e49bfb3e958
KhamzinAdel <khamzin.adel@mail.ru>
2022-12-31 16:16:22 +0300

    html

--- app/models.py
+++ app/models.py
@@ -4,7 +4,7 @@ from app import db
 class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(50), unique=True)
-    generation_code = db.Column(db.String(50), nullable=True)
+    generation_code = db.Column(db.String(500), nullable=True)
 
     def __repr__(self):
         return f'<users> {self.id}'
